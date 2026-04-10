2de20344d16ba88f6bd4612922ddfc6970484708
Specy <specy.dev@gmail.com>
2026-04-10 12:26:27 +0200

    update deps

--- react/DiscernsChatbot.tsx
+++ react/DiscernsChatbot.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, type CSSProperties } from 'react';
+import { type CSSProperties, useEffect, useRef, useState } from 'react';
 import type { EnvironmentContext, RegisteredTool, WebsiteProtocol } from '../js';
 import { connectToDiscernsChatbot } from '../js';
 
